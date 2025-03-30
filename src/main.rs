929a71cdc350de96835d9a09c5beed0ad5b36b73
toftpokk <pakkapol.20100@gmail.com>
2025-03-30 10:26:00 +0700

    WIP: revise tab

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::path::PathBuf;
 
 use iced::{
     Element, Task, Theme, highlighter,
-    widget::{button, column, row, text_editor},
+    widget::{button, column, row, scrollable, text_editor},
 };
 use rfd::FileDialog;
 
@@ -15,6 +15,8 @@ enum Message {
     Edit(text_editor::Action),
     SelectFile,
     ChangeTab(usize),
+    TabScroll(scrollable::Id, f32, f32),
+    TabSelected(usize),
 }
 
 fn main() -> Result<(), iced::Error> {
@@ -25,7 +27,6 @@ fn main() -> Result<(), iced::Error> {
 
 struct App {
     value: u64,
-    text_content: text_editor::Content,
     working_dir: Option<PathBuf>,
     tabs: tab::TabView,
 }
@@ -35,9 +36,6 @@ impl App {
         (
             App {
                 value: 1,
-                text_content: text_editor::Content::with_text(
-                    "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23\n24\n25\n26\n27\n28\n29\n30\n31\n32\n33\n34\n35\n36\n37\n38\n39\n40\n41\n42\n43\n44\n45\n46\n47\n48\n49\n50\n51\n52\n53\n54\n55\n56\n57\n58\n59\n60\n61\n62\n63\n64\n65\n66\n67\n68\n69\n70\n71\n72\n73\n74\n75\n76\n77\n78\n79\n80\n81\n82\n83\n84\n85\n86\n87\n88\n89\n90\n9",
-                ),
                 working_dir: None,
                 tabs: tab::TabView::new(),
             },
@@ -53,7 +51,7 @@ impl App {
             }
             Message::Edit(action) => match action {
                 text_editor::Action::Scroll { .. } => (),
-                _ => self.text_content.perform(action),
+                _ => self.tabs.perform(action),
             },
             Message::SelectFile => {
                 let mut tab = tab::Tab::new();
@@ -63,9 +61,9 @@ impl App {
                 }
                 self.tabs.push(tab);
             }
-            Message::ChangeTab(tab_index) => {
-                self.tabs.change_tab(tab_index);
-            }
+            Message::ChangeTab(tab_index) => return self.tabs.change_tab(tab_index),
+            Message::TabScroll(id, x, y) => (self.tabs.set_offset(id, x, y)),
+            Message::TabSelected(tab) => self.tabs.select(tab),
         }
 
         Task::none()
--- src/tab.rs
+++ src/tab.rs
@@ -1,11 +1,15 @@
 use std::{ffi, fs, path};
 
-use iced::widget::{Row, button, column, container, row, scrollable, text, text_editor};
-use iced::{Alignment, Element, Font, Length, Padding, Pixels, highlighter};
+use iced::widget::scrollable::RelativeOffset;
+use iced::widget::{
+    Row, Scrollable, button, column, container, row, scrollable, text, text_editor,
+};
+use iced::{Alignment, Element, Font, Length, Padding, Pixels, Task, highlighter};
 
 use crate::Message;
 
 pub struct TabView {
+    active_tab: usize,
     current_tab: Option<usize>,
     tabs: Vec<Tab>,
 }
@@ -13,6 +17,7 @@ pub struct TabView {
 impl TabView {
     pub fn new() -> Self {
         Self {
+            active_tab: usize::default(),
             current_tab: None,
             tabs: Vec::new(),
         }
@@ -23,76 +28,69 @@ impl TabView {
         self.current_tab = Some(self.tabs.len() - 1);
     }
 
-    pub fn change_tab(&mut self, tab_index: usize) {
+    pub fn change_tab(&mut self, tab_index: usize) -> Task<Message> {
         if tab_index < self.tabs.len() {
             self.current_tab = Some(tab_index);
+            // let tab = &self.tabs[tab_index];
+            // return scrollable::snap_to(tab.scroll_id.clone(), tab.scroll_offset);
         }
+        Task::none()
+    }
+
+    pub fn select(&mut self, selected: usize) {
+        self.active_tab = selected
     }
 
     pub fn view(&self) -> Element<Message> {
-        let font_size = 15.0;
-        let line_height = 1.1;
-        let syntax_theme = highlighter::Theme::SolarizedDark;
-
         let main = if let Some(tab_index) = self.current_tab {
             let tab = &self.tabs[tab_index];
-            row![
-                line_number(tab.content.line_count(), font_size, line_height,),
-                text_editor(&tab.content)
-                    .font(Font::MONOSPACE)
-                    .size(font_size)
-                    .line_height(line_height)
-                    .padding(Padding {
-                        top: 0.0,
-                        bottom: 0.0,
-                        left: 5.0,
-                        right: 0.0,
-                    })
-                    .highlight(
-                        tab.file_path
-                            .as_deref()
-                            .and_then(path::Path::extension)
-                            .and_then(ffi::OsStr::to_str)
-                            .unwrap_or(""),
-                        syntax_theme,
-                    )
-                    .on_action(Message::Edit)
-            ]
+            tab.view()
         } else {
-            Row::new()
+            scrollable(Row::new())
         };
 
-        let tab_list = self
-            .tabs
-            .iter()
-            .enumerate()
-            .fold(Row::new(), |row, (index, tab)| {
-                let file_name = if let Some(path) = &tab.file_path {
-                    path.file_name()
-                        .expect("invalid file")
-                        .to_str()
-                        .expect("invalid file string")
-                } else {
-                    ""
-                };
-                row.push(button(file_name).on_press(Message::ChangeTab(index)))
-            });
+        // let tab_list = self
+        //     .tabs
+        //     .iter()
+        //     .enumerate()
+        //     .fold(Row::new(), |row, (index, tab)| {
+        //         let file_name = if let Some(path) = &tab.file_path {
+        //             path.file_name()
+        //                 .expect("invalid file")
+        //                 .to_str()
+        //                 .expect("invalid file string")
+        //         } else {
+        //             ""
+        //         };
+        //         row.push(button(file_name).on_press(Message::ChangeTab(index)))
+        //     });
 
-        column![
-            tab_list,
-            scrollable(main)
-                .height(Length::Fill)
-                .direction(scrollable::Direction::Vertical(
-                    scrollable::Scrollbar::default().scroller_width(0).width(0),
-                )),
-        ]
-        .into()
+        column![iced_aw::TabBar::new(Message::TabSelected), main].into()
+    }
+
+    pub fn perform(&mut self, action: text_editor::Action) {
+        if let Some(tab_index) = self.current_tab {
+            self.tabs[tab_index].content.perform(action);
+        }
+    }
+
+    https://github.com/iced-rs/iced_aw/blob/main/examples/tab_bar.rs
+
+    pub fn set_offset(&mut self, id: scrollable::Id, x: f32, y: f32) {
+        // for tab in &mut self.tabs {
+        //     if tab.scroll_id == id {
+        //         tab.scroll_offset.x = x;
+        //         tab.scroll_offset.y = y;
+        //     }
+        // }
     }
 }
 
 pub struct Tab {
     file_path: Option<path::PathBuf>,
     content: text_editor::Content,
+    scroll_id: scrollable::Id,
+    scroll_offset: scrollable::RelativeOffset,
 }
 
 impl Tab {
@@ -100,6 +98,8 @@ impl Tab {
         Self {
             file_path: None,
             content: text_editor::Content::new(),
+            scroll_id: scrollable::Id::unique(),
+            scroll_offset: scrollable::RelativeOffset::default(),
         }
     }
 
@@ -120,6 +120,43 @@ impl Tab {
                 log::error!("Could not load file {:?}: {}", file_path, err)
             }
         }
+    }
+
+    pub fn view(&self) -> Scrollable<Message> {
+        let font_size = 15.0;
+        let line_height = 1.1;
+        let syntax_theme = highlighter::Theme::SolarizedDark;
+
+        Scrollable::new(row![
+            line_number(self.content.line_count(), font_size, line_height,),
+            text_editor(&self.content)
+                .font(Font::MONOSPACE)
+                .size(font_size)
+                .line_height(line_height)
+                .padding(Padding {
+                    top: 0.0,
+                    bottom: 0.0,
+                    left: 5.0,
+                    right: 0.0,
+                })
+                .highlight(
+                    self.file_path
+                        .as_deref()
+                        .and_then(path::Path::extension)
+                        .and_then(ffi::OsStr::to_str)
+                        .unwrap_or(""),
+                    syntax_theme,
+                )
+                .on_action(Message::Edit)
+        ])
+        .height(Length::Fill)
+        .direction(scrollable::Direction::Vertical(
+            scrollable::Scrollbar::default().scroller_width(0).width(0),
+        ))
+        .on_scroll(|viewport| {
+            let RelativeOffset { x, y } = viewport.relative_offset();
+            Message::TabScroll(self.scroll_id.clone(), x, y)
+        })
     }
 }
 
